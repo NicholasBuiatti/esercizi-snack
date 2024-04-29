e464f47b321d1788c9c6d4270d8ff9bb6690331c
Nicholas Buiatti <160488490+NicholasBuiatti@users.noreply.github.com>
2024-04-29 09:39:10 +0200

    Add files via upload

---
+++ esercizi snack/script.js
@@ -0,0 +1,53 @@
+//1)
+// const firstWord = prompt('inserisci una parola');
+// const secondWord = prompt('inserisci un\'altra parola');
+
+// if ( firstWord.length > secondWord.length) {
+//     alert(secondWord + firstWord)
+// } else {
+//     alert(firstWord + secondWord)
+// }
+
+//2)
+// let numSum = 0;
+// for (let i = 0; i < 3; i++) {
+//     let num = parseInt(prompt('inserisci un numero'))
+//     console.log(num, typeof num);
+//     numSum += num ;
+//     console.log(numSum);
+// }
+
+//3)
+// let risultato = 0;
+// let x = 2;
+
+// while ( risultato <= 1000) {
+//     risultato = 2 ** x;
+    
+//     console.log(`2 alla ${x} = ${risultato}`)
+//     x++;
+// }
+
+//4)
+// let ripetizioni = parseInt(prompt('per quante volte?'))
+// let x = 2;
+// let risultato;
+
+// for (let i = 0; i < ripetizioni; i++) {
+//     risultato = x ** ripetizioni; 
+//     console.log(x, risultato);
+//     x++;
+// }
+
+//5)
+// let num = prompt('scrivi un numero a 4 cifre');
+// let lista = num.split('');
+// let somma = 0;
+// console.log(lista);
+// for (let i = 0; i < lista.length; i++) {
+//     let element = parseInt(lista[i]);
+//     console.log(element, typeof element);
+//     somma += element;
+//     console.log(somma);
+// }
+
